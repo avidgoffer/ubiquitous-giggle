e0b00ff536ed728b39d4b2ed3de1f8fd07a947fa
Aaron Chancey <achancey@quicksolutions.com>
2015-12-03 21:26:11 -0500

    add test for 40 and 50.  also refactor to re-use code.

--- app/src/main/java/kata/romannumeralconverter/ArabicToRomanNumeralConverter.java
+++ app/src/main/java/kata/romannumeralconverter/ArabicToRomanNumeralConverter.java
@@ -1,33 +1,61 @@
 package kata.romannumeralconverter;
 
+import android.support.annotation.NonNull;
+
 public class ArabicToRomanNumeralConverter {
+
+    private int _numberToConvert;
+
     public String convert(final int numberToConvert) {
+        if(numberToConvert <= 0 || numberToConvert > 300) throw new IllegalArgumentException("numberToConvert is not in an expected range of 1 to 3,000");
+
+        _numberToConvert = numberToConvert;
+        StringBuilder romanNumerals = new StringBuilder();
+        romanNumerals.append(getNumerals("X", "L", "C", 10, 50, 100));
+        romanNumerals.append(getNumerals("I", "V", "X", 1, 5, 10));
+        return romanNumerals.toString();
+    }
+
+    @NonNull
+    private String getNumerals(final String lowNumeral, final String midNumeral, final String highNumeral,
+                               final int lowValue, final int midValue, final int highValue) {
         StringBuilder romanNumerals = new StringBuilder();
         int iCount = 0;
-        int number = numberToConvert;
-        while(number > 0) {
-            if(number - 5 >= 0 && number - 5 <= 3) {
-                number -= 5;
-                romanNumerals.append("V");
-            } else if(number - 5 >= 0 && number - 5 > 3) {
-                number -= 9;
-                romanNumerals.append("IX");
-            } else if(number - 1 >= 0) {
-                number -= 1;
+        int number = _numberToConvert;
+
+        while(number >= lowValue) {
+            if(number - highValue >= 0) {
+                number -= highValue;
+                romanNumerals.append(highNumeral);
+            } else if(number - midValue >= 0 && number - midValue <= lowValue * 3) {
+                number -= midValue;
+                romanNumerals.append(midNumeral);
+            } else if(number - midValue >= 0 && number - midValue > lowValue * 3) {
+                number -= highValue - 1;
+                romanNumerals
+                        .append(lowNumeral)
+                        .append(highNumeral);
+            } else if(number - lowValue >= 0) {
+                number -= lowValue;
                 iCount++;
                 if(iCount > 3){
                     romanNumerals.delete(romanNumerals.length() - 3, romanNumerals.length());
                     if(number > 0) {
-                        romanNumerals.append("IX");
+                        romanNumerals
+                                .append(midNumeral)
+                                .append(lowNumeral);
                     } else {
-                        romanNumerals.append("IV");
+                        romanNumerals
+                                .append(lowNumeral)
+                                .append(midNumeral);
                     }
                     iCount = 0;
                 } else {
-                    romanNumerals.append("I");
+                    romanNumerals.append(lowNumeral);
                 }
             }
         }
+        _numberToConvert = number;
         return romanNumerals.toString();
     }
 }
--- app/src/test/java/kata/romannumeralconverter/ArabicToRomanNumeralConverterTest.java
+++ app/src/test/java/kata/romannumeralconverter/ArabicToRomanNumeralConverterTest.java
@@ -12,8 +12,24 @@ public class ArabicToRomanNumeralConverterTest {
         assertEquals("II", arabicToRomanNumeralConverter.convert(2));
         assertEquals("III", arabicToRomanNumeralConverter.convert(3));
         assertEquals("IV", arabicToRomanNumeralConverter.convert(4));
-        assertEquals("V", arabicToRomanNumeralConverter.convert(5));
         assertEquals("VI", arabicToRomanNumeralConverter.convert(6));
+        assertEquals("V", arabicToRomanNumeralConverter.convert(5));
+        assertEquals("VII", arabicToRomanNumeralConverter.convert(7));
+        assertEquals("VIII", arabicToRomanNumeralConverter.convert(8));
         assertEquals("IX", arabicToRomanNumeralConverter.convert(9));
+        assertEquals("X", arabicToRomanNumeralConverter.convert(10));
+        assertEquals("XI", arabicToRomanNumeralConverter.convert(11));
+        assertEquals("XII", arabicToRomanNumeralConverter.convert(12));
+        assertEquals("XIII", arabicToRomanNumeralConverter.convert(13));
+        assertEquals("XIV", arabicToRomanNumeralConverter.convert(14));
+        assertEquals("XV", arabicToRomanNumeralConverter.convert(15));
+        assertEquals("XVI", arabicToRomanNumeralConverter.convert(16));
+        assertEquals("XVII", arabicToRomanNumeralConverter.convert(17));
+        assertEquals("XVIII", arabicToRomanNumeralConverter.convert(18));
+        assertEquals("XIX", arabicToRomanNumeralConverter.convert(19));
+        assertEquals("XX", arabicToRomanNumeralConverter.convert(20));
+        assertEquals("XXI", arabicToRomanNumeralConverter.convert(21));
+        assertEquals("XL", arabicToRomanNumeralConverter.convert(40));
+        assertEquals("L", arabicToRomanNumeralConverter.convert(50));
     }
 }
